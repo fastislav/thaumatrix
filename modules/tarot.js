ed0a3e1d066915baa40611325bd39aa164dd880d
fastislav <chezachezastas@gmail.com>
2026-04-27 20:34:39 +0300

    Update tarot.js

--- modules/tarot.js
+++ modules/tarot.js
@@ -47,4 +47,23 @@ const CARDS = [
   { num: 6, name: "Влюбленные", desc: "Выбор сердца, любовь, гармония", archetypes: ["creator", "diplomat"] },
   { num: 7, name: "Колесница", desc: "Победа, движение, контроль", archetypes: ["warrior", "leader"] },
   { num: 8, name: "Сила", desc: "Внутренняя мощь, терпение", archetypes: ["warrior", "wise_one"] },
-  { num: 9, name: "Отшельник", desc: "Мудрость, уединение, поиск", archetypes: ["wise_one", "
+  { num: 9, name: "Отшельник", desc: "Мудрость, уединение, поиск", archetypes: ["wise_one", "mystic"] },
+  { num: 10, name: "Колесо Фортуны", desc: "Судьба, циклы, перемены", archetypes: ["mystic", "seeker"] },
+  { num: 11, name: "Справедливость", desc: "Баланс, карма, истина", archetypes: ["wise_one", "diplomat"] },
+  { num: 12, name: "Повешенный", desc: "Иной взгляд, жертва, пауза", archetypes: ["mystic", "wise_one"] },
+  { num: 13, name: "Смерть", desc: "Трансформация, конец старого", archetypes: ["warrior", "mystic"] },
+  { num: 14, name: "Умеренность", desc: "Гармония, терпение, золотая середина", archetypes: ["diplomat", "creator"] },
+  { num: 15, name: "Дьявол", desc: "Искушение, страсть, зависимости", archetypes: ["warrior", "creator"] },
+  { num: 16, name: "Башня", desc: "Разрушение старого, озарение", archetypes: ["warrior", "leader"] },
+  { num: 17, name: "Звезда", desc: "Надежда, вдохновение, исцеление", archetypes: ["creator", "mystic"] },
+  { num: 18, name: "Луна", desc: "Иллюзии, подсознание, страхи", archetypes: ["mystic", "wise_one"] },
+  { num: 19, name: "Солнце", desc: "Радость, успех, ясность", archetypes: ["leader", "creator"] },
+  { num: 20, name: "Суд", desc: "Возрождение, призыв, прощение", archetypes: ["wise_one", "warrior"] },
+  { num: 21, name: "Мир", desc: "Завершение, целостность, путешествие", archetypes: ["creator", "diplomat"] },
+  { num: 22, name: "Шут", desc: "Начало, свобода, спонтанность", archetypes: ["creator", "seeker"] }
+];
+
+function parseDate(str) {
+  const [y,m,d] = str.split('-').map(Number);
+  return {year:y, month:m, day:d};
+}
